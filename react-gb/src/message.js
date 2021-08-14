1539d475e20ecf24c197860dbb1e650f6b93aec5
Olga <astanacards001@gmail.com>
2021-08-14 18:08:36 +0300

    Lesson 2

--- react-gb/src/message.js
+++ react-gb/src/message.js
@@ -4,7 +4,7 @@ function Message (props){
     return (
         <div className = "message">
             <h3 className = "author"> от {props.author} </h3>
-        <p className = "body_message"> {props.body} </p>
+        <p className = "body_message"> {props.text} </p>
             </div>
         
     )
